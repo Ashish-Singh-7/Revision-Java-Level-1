6bbc3909e1c2baf7f9f3498c6bd4f4f35905aa5c
Ashish-Singh-7 <110215670+Ashish-Singh-7@users.noreply.github.com>
2022-09-17 23:30:38 +0530

    Add files via upload

---
+++ pattern4.java
@@ -0,0 +1,27 @@
+public class Main {
+
+    public static void main(String[] args) {
+    Scanner scn = new Scanner(System.in);
+    
+    // write ur code here
+    int n=scn.nextInt();
+    
+    int space=0;
+    int star=n;
+    
+    for(int i=1;i<=n;i++)
+    {
+    for(int j=1;j<=space;j++)
+    {
+    System.out.print(" ");
+    }
+    for(int j=1;j<=star;j++)
+    {
+    System.out.print("* ");
+    }
+    space++;
+    star--;
+    System.out.println();
+    }
+    }
+    }
