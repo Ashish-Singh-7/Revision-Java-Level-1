047ac6d43f152304ff353cb4ddb3b5eaef329de0
Ashish-Singh-7 <110215670+Ashish-Singh-7@users.noreply.github.com>
2022-09-20 11:47:01 +0530

    Add files via upload

---
+++ pattern10.java
@@ -0,0 +1,38 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Scanner scn=new Scanner(System.in);
+        int n=scn.nextInt();
+
+        int outer_space=n/2;
+        int inner_space=-1;
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=outer_space;j++)
+            {
+                System.out.print("\t");
+            }
+            System.out.print("*\t");
+
+            for(int j=1;j<=inner_space;j++)
+            {
+                System.out.print("\t");
+            }
+            if(i>1 && i<n)
+            {
+                System.out.print("*\t");
+            }
+            if(i<=n/2)
+            {
+                outer_space--;
+                inner_space+=2;
+            }
+            else
+            {
+                outer_space++;
+                inner_space-=2;
+            }
+        }
+        System.out.println();
+    }
+}
