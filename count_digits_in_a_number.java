efb373ed9aa8e69e92a4a705e635204fdb1cd0ff
Ashish-Singh-7 <110215670+Ashish-Singh-7@users.noreply.github.com>
2022-10-22 23:40:23 +0530

    Add files via upload

--- count_digits_in_a_number.java
+++ count_digits_in_a_number.java
@@ -1,16 +1,15 @@
-import java.util.Scanner;
-
+import java.util.*;
 public class count_digits_in_a_number{
     public static void main(String[] args){
         Scanner scn=new Scanner(System.in);
         int n=scn.nextInt();
 
-        int no_of_dig=0;
+        int dig=n%10;
         while(n!=0)
         {
             n=n/10;
-            nod++;
+            dig++;
         }
-        System.out.print(no_of_dig);
+        System.out.println(dig);
     }
 }
