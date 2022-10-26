854c116cfd0f5449c36c2aa9d0b663f02e9ceda1
Ashish-Singh-7 <110215670+Ashish-Singh-7@users.noreply.github.com>
2022-10-26 11:27:50 +0530

    Add files via upload

--- reverse_a_number.java
+++ reverse_a_number.java
@@ -1,9 +1,10 @@
-public class reverse_a_number{
+import java.util.*;
+public class reverse_a_number {
     public static void main(String[] args){
         Scanner scn=new Scanner(System.in);
         int n=scn.nextInt();
         
-        while(n>0)
+        while(n!=0)
         {
             int dig=n%10;
             n=n/10;
