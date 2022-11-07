2e1083ada7d53460df3645b12063bc27cb905b55
Ashish-Singh-7 <110215670+Ashish-Singh-7@users.noreply.github.com>
2022-11-07 19:03:02 +0530

    Add files via upload

--- decimal_to_any_base.java
+++ decimal_to_any_base.java
@@ -1,24 +1,25 @@
 import java.util.*;
+public class decimal_to_any_base{
 
-public class Main {
-  public static void main(String[] args) {
+    public static void main(String[] args) {
     Scanner scn = new Scanner(System.in);
-    int n = scn.nextInt();
-    int b = scn.nextInt();
-    System.out.println(getValueInBase(n, b));
-    scn.close();
-  }
-
-  public static int getValueInBase(int n, int b) {
-    int rv = 0;
-    int power = 1;
-    while (n > 0)
-    {
-      int remainder = n % b;
-      n = n / b;
-      rv = rv + remainder * power;
-      power *= 10;
+    int number = scn.nextInt();
+    int base = scn.nextInt();
+    int result = getValueInBase(number, base);
+    System.out.println(result);
     }
-    return rv;
-  }
-}
+    
+    public static int getValueInBase(int n, int b){
+    // write code here
+    
+    int answer=0;int power=1;
+    while(n>0)
+    {
+    int rem=n%b;
+    n=n/b;
+    answer=answer + rem * power;
+    power*=10;
+    }
+    return answer;
+    }
+    }
