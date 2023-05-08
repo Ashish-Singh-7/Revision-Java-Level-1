2c2fe348af63cfef6e24189c6c2a7cee4834b376
Ashish-Singh-7 <110215670+Ashish-Singh-7@users.noreply.github.com>
2023-05-08 19:43:54 +0530

    Add files via upload

---
+++ Domino_piling_practice.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Domino_piling_practice{
+    public static void main(String[] args){
+        Scanner scn=new Scanner(System.in);
+        int m=scn.nextInt();
+        int n=scn.nextInt();
+        int area=m*n/2;
+        System.out.println(area);
+    }
+}
