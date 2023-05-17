d6c76302d069ca87b11adc1105aa96cd63286880
Ashish-Singh-7 <110215670+Ashish-Singh-7@users.noreply.github.com>
2023-05-17 19:30:26 +0530

    Add files via upload

--- practice.java
+++ practice.java
@@ -3,18 +3,6 @@ public class practice{
     public static void main(String[] args){
         Scanner scn=new Scanner(System.in);
         int n=scn.nextInt();
-        int count=0;
-        while(n-->0)
-        {
-            int a=scn.nextInt();
-            int b=scn.nextInt();
-            int c=scn.nextInt();
-            int sum=a+b+c;
-            if(sum>=2)
-            {
-                count++;
-            }
-        }
-        System.out.println(count);
+        for(int i=0;i<)
     }
 }
