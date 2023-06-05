7e0f8dd79139c0f6a5b3685221f66c5639abdae0
Ashish-Singh-7 <110215670+Ashish-Singh-7@users.noreply.github.com>
2023-06-06 01:04:05 +0530

    Add files via upload

---
+++ Word_Capitalization_practice2.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Word_Capitalization_practice2{
+    public static void main(String[] args){
+        Scanner scn=new Scanner(System.in);
+        String str=scn.next();
+        String res=str.substring(0,1).toUpperCase()+str.substring(1);
+        System.out.println(res);
+    }
+}
